66e4955a0e12ceacfc2a80d40ac7aee4c9a23fdb
Hez Ronningen <code@hez.ca>
2019-04-22 12:53:21 -0700

    Include user helpers

---
+++ lib/pxu_auth0/user_helpers.ex
@@ -0,0 +1,11 @@
+defmodule PxUAuth0.UserHelpers do
+  @moduledoc """
+  Conveniences for Auth0 authenication.
+  """
+
+  def current_user(conn), do: conn.assigns[:current_user]
+
+  def logged_in?(conn), do: Map.has_key?(conn.assigns, :current_user)
+
+  def in_group?(conn, group_name), do: conn |> current_user() |> Map.get(:groups, []) |> Enum.member?(group_name)
+end
