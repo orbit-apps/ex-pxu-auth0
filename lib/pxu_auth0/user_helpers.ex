77843356066549b0bea0a955c99b3a5804a06b46
Scott Mahr <essmahr@gmail.com>
2019-04-29 13:28:25 -0700

    Add user avatar helper

--- lib/pxu_auth0/user_helpers.ex
+++ lib/pxu_auth0/user_helpers.ex
@@ -1,7 +1,9 @@
 defmodule PxUAuth0.UserHelpers do
   @moduledoc """
-  Conveniences for Auth0 authenication.
+  Conveniences for Auth0 authentication.
   """
+
+  @fallback_avatar "https://twemoji.maxcdn.com/2/72x72/1f913.png"
 
   def current_user(conn), do: conn.assigns[:current_user]
 
@@ -9,4 +11,11 @@ defmodule PxUAuth0.UserHelpers do
 
   def in_group?(conn, group_name),
     do: conn |> current_user() |> Map.get(:groups, []) |> Enum.member?(group_name)
+
+  def user_avatar(conn) do
+    case current_user(conn) do
+      %{avatar: ""} -> @fallback_avatar
+      %{avatar: avatar} -> avatar
+    end
+  end
 end
