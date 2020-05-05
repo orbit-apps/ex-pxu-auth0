b142a13d5727915570564e4c0091b778aa28b554
Hez Ronningen <code@hez.ca>
2020-05-05 09:53:46 -0700

    move to github actions and bump elixir ver

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PxUAuth0.MixProject do
     [
       app: :pxu_auth0,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
